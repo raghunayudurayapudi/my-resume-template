1fbdff4a3d9b75320a8f1c5e63706d13c0aa8b4f
apple <raghu.kpf189@gmail.com>
2020-03-23 01:17:51 -0500

    Updates

---
+++ precache-manifest.3b29acb5f6a8b17252b2a50c5693bf1a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ef95dfdcf62035ec1b36",
+    "url": "/raghunayudurayapudi/my-resume-template.git/static/css/main.8894b739.chunk.css"
+  },
+  {
+    "revision": "ef95dfdcf62035ec1b36",
+    "url": "/raghunayudurayapudi/my-resume-template.git/static/js/main.9887c72e.chunk.js"
+  },
+  {
+    "revision": "c01f5ee2c277fad14b55",
+    "url": "/raghunayudurayapudi/my-resume-template.git/static/js/runtime~main.84c83f55.js"
+  },
+  {
+    "revision": "ae62c6e6aae1bc3bd70f",
+    "url": "/raghunayudurayapudi/my-resume-template.git/static/css/2.72b3ee53.chunk.css"
+  },
+  {
+    "revision": "ae62c6e6aae1bc3bd70f",
+    "url": "/raghunayudurayapudi/my-resume-template.git/static/js/2.b245f0ee.chunk.js"
+  },
+  {
+    "revision": "1383d4146e5103644389b08c8a6263f1",
+    "url": "/raghunayudurayapudi/my-resume-template.git/index.html"
+  }
+];
